2a83bd3deb80643c55acc2726c8beff4657ba41f
Millard1018 <millardlirio@gmail.com>
2025-01-09 10:54:25 +0800

    commit

---
+++ ChatBotDraft.py
@@ -0,0 +1,13 @@
+class chatBotData:
+    def __init__(self, question, response):
+        self.question = question
+        self.response = response
+
+def user_input()->str:
+    pass
+
+def main()->None:
+    pass
+
+if __name__ == '__main__':
+    main()
