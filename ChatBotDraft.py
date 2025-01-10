7eeca1174bb2df94eb85850d822eae6000fcfa95
Millard1018 <millardlirio@gmail.com>
2025-01-10 11:32:26 +0800

    commit

--- ChatBotDraft.py
+++ ChatBotDraft.py
@@ -1,61 +1,53 @@
 from spellchecker import SpellChecker
 
 class chatBotData:
-    def __init__(self, word):
-        self.word = word
+    def __init__(self, sentence):
+        self.sentence = sentence
 
     def reply(self):
         #SAMPLE NOT FINAL
-        data_keywords = []
-        values = []
-
         data = {
             "HOW ARE YOU": ["Fine, Thank you for asking!", "Doing Well", "Great, How about you?"]
             #We can add more reply and question
         }
-        noOfWords = {
+        no_of_words = {
             "HOW ARE YOU": 3
         }
 
-        for key in data.keys():
+        '''for key in data.keys():
             data_keywords.append(key.split()) # If word is different but same meaning(synonyms) we can add that too
-        # We can compare a specific word in  words if it has a synonym in the keywords which will count in that specific key
+        # We can compare a specific word in  words if it has a synonym in the keywords which will count in that specific key'''
 
-        compare = { # Will compare matches it got
-            "HOW ARE YOU": 0
-        }
+        compare = {key: 0 for key in data.keys()} # Will compare matches it got
         
-        for word in (data_keywords):
-            if self.word == word:
-                compare[word] += 1
+        input_words = set(self.sentence.split())
+        for key, responses in data.items():
+            key_words = set(key.split())
+            compare[key] = len(input_words & key_words)  # Count matching words
 
         highest_match = max(compare.values())
-        keys = [k for k, v in data.items() if v == highest_match]
+        if highest_match == 0:
+            print("chatbot name: I'm sorry, I don't understand that.")
+            return
 
-        if len(keys) == 1:
-            print(f"chatbot name: {data[keys]}")
-        else: # If there is a tie
-            for key in keys:
-                value = noOfWords[key]//highest_match  # if it matches 80% of the words in the sentence then it is assumpted that that will be the correct reply
-                valuesdict = {key: value}
-                values.append(valuesdict)
-            single_dict: dict = max(values, key=lambda x: list(x.values())[0])
-            key = single_dict.keys()[0]
-            print(f"chatbot name: {data[key]}")
+        matching_keys = [k for k, v in compare.items() if v == highest_match]
+
+        if len(matching_keys) == 1:
+            print(f"chatbot name: {data[matching_keys[0]][0]}")
+        else:
+            # If their is a tie
+            best_match = max(
+                matching_keys,
+                key=lambda k: len(input_words & set(k.split())) / no_of_words[k]
+            )
+            print(f"chatbot name: {data[best_match][0]}")
 
 
-def validation(words: list)->list: # To suggest the most simillar word to the wrongly spelt word
-    dictionary = SpellChecker()
-    correct_words = []
-    final_words = []
-    for word in words:
-        if word not in dictionary:
-            correct_words.append(dictionary.candidate(word))
-    
-    for word in correct_words:
-        final_words.append(chatBotData(word))
-
-    return final_words
+def validation(sentence: str)->str: # To suggest the most simillar word to the wrongly spelt word
+    spell = SpellChecker()
+    words = sentence.split()
+    corrected_words = [spell.correction(word) for word in words]
+    return " ".join(corrected_words)
 
 def introduction()->None:
     print("chatbot name: Hi my name is -----")
@@ -64,21 +56,19 @@ def introduction()->None:
 
 def user_input()->str:
     question: str = input("user: ").upper()
-    words = validation(question)
-
-    return words
-def chatBotReply(words: list)-> str:
-    words.reply
+    if question == 'EXIT':
+        return 'EXIT'
+    return validation(question)
 
 def main()->None:
     introduction()
-    while True: 
-        words = user_input()
-        if words == 'EXIT':
+    while True:
+        sentence = user_input()
+        if sentence == 'EXIT':
             print("chatbot name: Thank you for chatting with me!")
-            quit()
-        else:
-            chatBotReply(words)
+            break
+        bot = chatBotData(sentence)
+        bot.reply()
 
 if __name__ == '__main__':
     main()
